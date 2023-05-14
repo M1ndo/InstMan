a3e476a37e1418f4a1d3ddc94bbe6a8ecb47bd24
AlienX <AlienX@duck.com>
2023-05-15 00:06:13 +0100

    [new] Adding posts track mechanism
    Track deleted/new posts
    Fix repeatable json.
    [untracked] Highlights and stories.

--- src/instman.py
+++ src/instman.py
@@ -14,6 +14,8 @@ from multiprocessing.pool import ThreadPool
 from args_format import arguments
 from postdata import GetInfo
 import yaml, logging, json, time
+from pdb import set_trace as bp
+
 
 date_time = datetime.now().strftime("%Y/%-m/%-d :: %H:%M:%S")
 users_file = Path("~/.config/instman/users.yml").expanduser()
@@ -24,6 +26,7 @@ class InstMan():
         self.username = username
         self.password = password
         self.api = instaloader.Instaloader(quiet=debug) # type: ignore
+        self.profile = None
         self.load_session()
         # self.api.load_session_from_file(self.username)
 
@@ -97,7 +100,7 @@ class InstMan():
         """ Create a user file that contains its full information """
         user_file = Path(f"~/.config/instman/userdata/{userid}/data.yml").expanduser()
         user_mark = Path(f"~/.config/instman/userdata/{userid}/changes.yml").expanduser()
-        user_post = Path(f"~/.config/instman/userdata/{userid}/postdata.yml").expanduser()
+        user_post = Path(f"~/.config/instman/userdata/{userid}/postdata.json").expanduser()
         user_file.parent.mkdir(parents=True,exist_ok=True)
         user_file.touch()
         user_mark.touch()
@@ -115,6 +118,16 @@ class InstMan():
             return None,None
         exist = userid in data
         return data,exist
+
+    def read_write(self, file, data, mode='r'):
+        """ read And write data in json format """
+        with open(file, mode) as fdata:
+            if mode=='r':
+                try:
+                    return json.load(fdata)
+                except:
+                    return None
+            json.dump(data, fdata)
 
     def user_data(self, users):
         """ Construct a set of data of a user and save it """
@@ -234,34 +247,10 @@ class InstMan():
                 logging.info(f"Downloading {media_title} total items [{media.itemcount}].")
                 with tqdm(total=media.itemcount, desc=f"Downloading {media_type} {media_title}", colour="#6a5acd", unit_scale=True, unit="MB", smoothing=0.5) as pbar:
                     for item in media.get_items():
-                        path = f"media/{media.owner_username}/{media_type.lower()}s/" if media_type == "Story" else f"media/{media.owner_username}/{media_type.lower()}s/{media.title}/"
+                        path = f"media/{media.owner_username}/stories/" if media_type == "Story" else f"media/{media.owner_username}/highlights/{media.title}/"
                         if self.api.download_storyitem(item=item, target=Path(path)):
                             pbar.update(1)
         return media_list
-
-    # def get_posts(self, profile: Profile, comments:bool=False, download:bool=False) -> Tuple[List[Post], List[PostComment]]:
-    #     """ Retrieve/Download User posts """
-    #     posts = [post for post in profile.get_posts()]
-    #     lcomments = []
-    #     if download:
-    #         logging.info(f"Downloading {profile.username} Posts [{len(posts)}].")
-    #         with tqdm(total=len(posts), desc=f"Downloading {profile.username} Posts [{len(posts)}]", colour="#008b8b", unit_scale=True, unit="MB", smoothing=0.5) as pbar:
-    #             count = 0
-    #             for post in posts:
-    #                 if comments:
-    #                     lcomments.append({"postcode": post.shortcode, "postcomment": []})
-    #                     logging.debug(f"Getting {post.caption} Comments")
-    #                     for comment in post.get_comments():
-    #                         lcomments[count]['postcomment'].append(comment)
-    #                 count += 1
-    #                 post_dir = post.date_local.strftime("%Y-%m-%d_%H%M%S")
-    #                 path = f"media/{profile.username}/posts/{post_dir}/"
-    #                 if self.api.download_post(post=post, target=Path(path)):
-    #                     pbar.update(1)
-    #                 else:
-    #                     logging.debug(f"Post {post.pcaption} is already downloaded.")
-    #                     pbar.update(1)
-    #     return posts, lcomments
 
     def get_posts(self, profile: Profile, comments:bool=False, download:bool=False) -> Tuple[List[Post], List[Dict[str, Union[str, List[PostComment]]]]]:
         """ Retrieve/Download User posts """
@@ -282,7 +271,6 @@ class InstMan():
         return posts, lcomments
 
     def download_post(self, post):
-        print(post)
         post_dir = post.date_local.strftime("%Y-%m-%d_%H%M%S")
         path = f"media/{post.owner.username}/posts/{post_dir}/"
         if self.api.download_post(post=post, target=Path(path)):
@@ -301,6 +289,7 @@ class InstMan():
     def list_media(self, user, highlights:bool=False, stories:bool=False, posts:bool=False, comments:bool=False, download:bool=False):
         """ List media of user including all their profile stories/reels/photos """
         profile = self.profile_access(user)
+        self.profile = profile
         self.api.save_metadata = True
         self.api.compress_json = False
         self.api.post_metadata_txt_pattern = ''
@@ -311,12 +300,18 @@ class InstMan():
         userid = [profile.userid]
         highlights_and_stories = self.get_stories_or_highlights(userid,highlights=highlights, stories=stories, download=download)
         if posts:
-            r_posts, r_comments = self.get_posts(profile, comments=True, download=download)
-            self.handle_posts(r_posts, r_comments)
-        # self.mark_changes(data=data,userid=str(userid[0]))
+            r_posts, r_comments = self.get_posts(profile, comments=False, download=download)
+            data = self.handle_posts(r_posts, r_comments)
+            self.mark_changes(data=data, type="posts", userid=str(userid[0]))
+        if highlights_and_stories:
+            for media in highlights_and_stories:
+                if isinstance(media, Story):
+                    print(media.latest_media_local)
+                    for item in highlights_and_stories[0].get_items():
+                        print(dir(item))
         return None
 
-    def handle_posts(self, posts: List[Post], comments: List[Dict[str, Union[str, List[PostComment]]]]):
+    def handle_posts(self, posts: List[Post], comments: List[Dict[str, Union[str, List[PostComment]]]]) -> dict:
         """ Handler for processing posts """
         data = {}
         if posts:
@@ -329,7 +324,7 @@ class InstMan():
                 post_comments = comments_by_postcode.get(post.shortcode, [])
                 likers = [like for like in post.get_likes()]
                 post_data = {
-                    "postdate": post.date_utc,
+                    "postdate": post.date_utc.strftime("%Y-%m-%d_%H%M%S"),
                     "caption": post.caption,
                     "caption_mentioned": post.caption_mentions,
                     "caption_hashtags": post.caption_hashtags,
@@ -346,16 +341,55 @@ class InstMan():
                 for com in post_comments:
                     for comment in com['postcomment']:
                         data[post.shortcode]['comments'][comment.id] = {comment.owner.username: comment.text}
+        return data
 
-    def mark_changes(self, data: List[dict], userid: str) -> None:
-        """ Mark Changes from stories/highlights/posts """
+    def media_changed(self, new_data: dict, old_data: dict, userid: str, type: str="posts"):
+        """ Track user media change from posts/stories/highlights """
+        if len(new_data) != len(old_data):
+            logging.info(f"Media {type} has been changed from {len(old_data)} to {len(new_data)}")
+            deleted = set(old_data.keys()).difference(new_data.keys())
+            new = set(new_data.keys()).difference(old_data.keys())
+            changed = [{type: len(new_data)}]
+            old_changed = [{type: len(old_data)}]
+            self.save_changes(changed, old_changed, userid)
+            return new, deleted
+        else:
+            logging.info(f"No {type} changes were found")
+            return False
+
+    def check_downloaded(self, date: str) -> bool:
+        """ Check if media is downloaded """
+        dir = Path(Path.cwd().parent.as_posix()+f"/media/{str(self.profile.username)}/{date}/") # type: ignore
+        if dir.is_dir():
+            return True
+        return False
+
+    def mark_changes(self, data: dict, type: str, userid: str) -> None:
+        """ Mark/Save Changes from stories/highlights/posts """
         _, _, post_file = self.user_file(userid)
-        post_data, _ = self.read_data(post_file, userid)
+        post_data = self.read_write(post_file, data=None) or {}
         if not post_data:
-            post_data = []
-        post_data.extend(data)
-        self.write_data_to_file(data=post_data, user_file=post_file)
-        return None
+            logging.info(f"No {type} metadata was saved yet, saving..")
+            post_data.update(data)
+            self.read_write(post_file, post_data, mode='w')
+            return
+        try:
+            new, deleted = self.media_changed(data, post_data, userid, type=type)  # type: ignore
+            if new:
+                post_data.update(data)
+                logging.info(f"{type} item {data['caption']} has been added to db")
+            if deleted:
+                deleted_post = post_data.pop(list(deleted)[0])
+                if self.check_downloaded(deleted_post['postdate']):
+                    logging.info(f"Luckily deleted {type} {deleted_post['caption']} was already downloaded")
+                else:
+                    logging.info(f"Unlucky deleted {type} {deleted_post['caption']} was not downloaded before")
+                    logging.info("But its metadata is still saved for data processing")
+                self.media_new({list(deleted)[0]: deleted_post}, userid)
+            self.read_write(post_file, post_data, mode='w')
+        except Exception as e:
+            logging.info(f"{type} items are already at their latest")
+            return
 
     def save_changes(self, changes, old_changes, userid):
         """ Save User Changes Into A File """
@@ -391,8 +425,17 @@ class InstMan():
             data = tmp
         if tmp is None and new_data is not None:
             data[userid][key].append(new_data)
-        with open(userfile, "w") as userdata:
-            json.dump(data, userdata)
+        self.read_write(userfile, data, mode='w')
+
+    def media_new(self, data, userid):
+        """ Save deleted metadata """
+        dfile = Path(f"~/.config/instman/userdata/{userid}/deleted_media.json").expanduser()
+        dfile.touch()
+        old_data, _ = self.read_data(dfile, userid)
+        if not old_data:
+            old_data = {}
+        old_data.update(data)
+        self.read_write(dfile, old_data, mode='w')
 
     def renew_data(self, data, userid, k, v):
         """ Renew old data once changes were made """
